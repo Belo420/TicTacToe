4fcdf8efa00e0fc037ea4d05395d7d1d80cf2283
Blazej Belowski <bbelowski@sii.pl>
2022-10-08 16:22:14 +0200

    update - Added diagonal condition (hard coded, dynamic wip) - Method updates, removed writelines from them

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -4,14 +4,8 @@
     {
         static void Main(string[] args)
         {
-            string[,] boardValues = new string[3, 3] { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" } };
-            DrawABoard(boardValues);
-            ChoicePlayerA(boardValues);
-            ChoicePlayerA(boardValues);
-            ChoicePlayerA(boardValues);
-            //ChoicePlayerB(boardValues);
-            CheckForWinCondition(boardValues);
-        }//suma wartosci 
+            Game();
+        }
         static internal void DrawABoard(string[,] boardValues)
         {
             Console.WriteLine("-------------------");
@@ -40,8 +34,6 @@
         }
         static internal void ChoicePlayerB(string[,] boardValues)
         {
-
-            Console.WriteLine("Podaj wartosc do zakreslenia");
             int choice = int.Parse(Console.ReadLine());
             for (int i = 0; i < boardValues.GetLength(0); i++)
                 for (int j = 0; j < boardValues.GetLength(1); j++)
@@ -54,16 +46,55 @@
                 }
             DrawABoard(boardValues);
         }
-        static internal bool CheckForWinCondition(string[,] boardValues)
+        static internal bool CheckForWinConditionRows(string[,] boardValues)
         {
-                if(boardValues[0, 0] == boardValues[0, 1] || boardValues[0, 1] == boardValues[0, 2])
-                    {
-                        Console.WriteLine("essa");
-                    }
-                else
+            for (int i = 0; i < boardValues.GetLength(0); i++)
+                if (boardValues[i, 0] == boardValues[i, 1] || boardValues[i, 1] == boardValues[i, 2])
+                {
+                    return true;
+                }
+            return false;
+        }
+        static internal bool CheckForWinConditionColumn(string[,] boardValues)
+        {
+            for (int i = 0; i < boardValues.GetLength(0); i++)
+                if (boardValues[0, i] == boardValues[1, i] || boardValues[1, i] == boardValues[2, i])
+                {
+                    return true;
+                }
+            return false;
+        }
+        static internal bool CheckForWinConditionDiagonal(string[,] boardValues)
+        {
+            if (boardValues[0, 0] == boardValues[1, 1] || boardValues[1, 1] == boardValues[2, 2])
             {
-                Console.WriteLine("pizda");
+                return true;
             }
+            if (boardValues[2, 0] == boardValues[1, 1] || boardValues[1, 1] == boardValues[0, 2])
+            {
+                return true;
+            }
+            return false;
+        }
+        static internal void Game()
+        {
+            string[,] boardValues = new string[3, 3] { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" } };
+            while (true)
+            {
+                DrawABoard(boardValues);
+                Console.WriteLine("Gracz 1 wybiera: ");
+                ChoicePlayerA(boardValues);
+                Console.WriteLine("Gracz 2 wybiera: ");
+                ChoicePlayerB(boardValues);
+                CheckForWinConditionRows(boardValues);
+                CheckForWinConditionColumn(boardValues);
+                CheckForWinConditionDiagonal(boardValues);
+                
+            }
+
+        }
+        static internal bool CheckForWin(string[,] boardValues)
+        {
             return false;
         }
     }
