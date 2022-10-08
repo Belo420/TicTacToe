6537d62c5d9fe40864d86602cfa6949dfd40f6c9
Blazej Belowski <bbelowski@sii.pl>
2022-10-08 14:49:53 +0200

    update - made a 1 condition for win (others wip)

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -7,17 +7,19 @@
             string[,] boardValues = new string[3, 3] { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" } };
             DrawABoard(boardValues);
             ChoicePlayerA(boardValues);
-            ChoicePlayerB(boardValues);
+            ChoicePlayerA(boardValues);
+            ChoicePlayerA(boardValues);
+            //ChoicePlayerB(boardValues);
             CheckForWinCondition(boardValues);
         }//suma wartosci 
         static internal void DrawABoard(string[,] boardValues)
         {
             Console.WriteLine("-------------------");
-            Console.WriteLine($" {boardValues[0,0]}    |  {boardValues[0,1]}     |  {boardValues[0,2]}   ");
+            Console.WriteLine($" {boardValues[0, 0]}    |  {boardValues[0, 1]}     |  {boardValues[0, 2]}   ");
             Console.WriteLine("-------------------");
-            Console.WriteLine($" {boardValues[1,0]}    |  {boardValues[1,1]}     |  {boardValues[1,2]}   ");
+            Console.WriteLine($" {boardValues[1, 0]}    |  {boardValues[1, 1]}     |  {boardValues[1, 2]}   ");
             Console.WriteLine("-------------------");
-            Console.WriteLine($" {boardValues[2,0]}    |  {boardValues[2,1]}     |  {boardValues[2,2]}   ");
+            Console.WriteLine($" {boardValues[2, 0]}    |  {boardValues[2, 1]}     |  {boardValues[2, 2]}   ");
             Console.WriteLine("-------------------");
         }
         static internal void ChoicePlayerA(string[,] boardValues)
@@ -25,14 +27,14 @@
 
             Console.WriteLine("Podaj wartosc do zakreslenia");
             int choice = int.Parse(Console.ReadLine());
-            for(int i = 0; i < boardValues.GetLength(0); i++)
-                for(int j = 0; j < boardValues.GetLength(1); j++)
+            for (int i = 0; i < boardValues.GetLength(0); i++)
+                for (int j = 0; j < boardValues.GetLength(1); j++)
                 {
                     if (boardValues[i, j] == choice.ToString())
                     {
                         boardValues[i, j] = "X";
                     }
-                    
+
                 }
             DrawABoard(boardValues);
         }
@@ -54,9 +56,13 @@
         }
         static internal bool CheckForWinCondition(string[,] boardValues)
         {
-            for (int i = 0; i < boardValues.Length; i++)
+                if(boardValues[0, 0] == boardValues[0, 1] || boardValues[0, 1] == boardValues[0, 2])
+                    {
+                        Console.WriteLine("essa");
+                    }
+                else
             {
-               
+                Console.WriteLine("pizda");
             }
             return false;
         }
