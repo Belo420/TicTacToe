43b96f22511cd7ee9d8207626790025da23b76f8
Belo420 <rakuuu1337@gmail.com>
2022-10-20 17:37:49 +0200

    JSON implemented - Added saving to JSON

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,10 +1,11 @@
-﻿namespace TicTacToe
+﻿using Newtonsoft.Json;
+
+namespace TicTacToe
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
             Game();
         }
         static internal void DrawABoard(string[,] boardValues)
@@ -79,37 +80,64 @@
         {
             string[,] boardValues = new string[3, 3] { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" } };
             DrawABoard(boardValues);
-            while (true)
+            Console.WriteLine("Wybierz opcje \n1. Nowa gra \n2. Statystyki \n3. Exit");
+            int menu = int.Parse(Console.ReadLine());
+            Scores scores = new Scores()
             {
-                Console.WriteLine("Gracz 1 wybiera: ");
-                ChoicePlayerA(boardValues);
-                if (CheckForWinConditionRows(boardValues) == true)
-                {
-                    Console.WriteLine("Wygrana przez rząd wierszy");
-                }
-                if (CheckForWinConditionDiagonal(boardValues) == true)
-                {
-                    Console.WriteLine("Wygrana przez przekątną");
-                }
-                if (CheckForWinConditionColumn(boardValues) == true)
-                {
-                    Console.WriteLine("Wygrana przez rząd kolumn");
-                }
-                Console.WriteLine("Gracz 2 wybiera: ");
-                ChoicePlayerB(boardValues);
-                if (CheckForWinConditionRows(boardValues) == true)
-                {
-                    Console.WriteLine("Wygrana przez rząd wierszy");
-                }
-                if (CheckForWinConditionDiagonal(boardValues) == true)
-                {
-                    Console.WriteLine("Wygrana przez przekątną");
-                }
-                if (CheckForWinConditionColumn(boardValues) == true)
-                {
-                    Console.WriteLine("Wygrana przez rząd kolumn");
-                }
+                Player1 = new Player1(),
+                Player2 = new Player2() 
+            };
+            using (StreamWriter file = File.CreateText(@"c:\Gry\scores.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, scores);
             }
+           
+            switch (menu)
+            {
+                case 1:
+                    {
+                        while (true)
+                        {
+                            Console.WriteLine("Gracz 1 wybiera: ");
+                            ChoicePlayerA(boardValues);
+                            if (CheckForWinConditionRows(boardValues) == true)
+                            {
+                                Console.WriteLine("Wygrana przez rząd wierszy");
+                                break;
+                            }
+                            if (CheckForWinConditionDiagonal(boardValues) == true)
+                            {
+                                Console.WriteLine("Wygrana przez przekątną");
+                                break;
+                            }
+                            if (CheckForWinConditionColumn(boardValues) == true)
+                            {
+                                Console.WriteLine("Wygrana przez rząd kolumn");
+                                break;
+                            }
+                            Console.WriteLine("Gracz 2 wybiera: ");
+                            ChoicePlayerB(boardValues);
+                            if (CheckForWinConditionRows(boardValues) == true)
+                            {
+                                Console.WriteLine("Wygrana przez rząd wierszy");
+                                break;
+                            }
+                            if (CheckForWinConditionDiagonal(boardValues) == true)
+                            {
+                                Console.WriteLine("Wygrana przez przekątną");
+                                break;
+                            }
+                            if (CheckForWinConditionColumn(boardValues) == true)
+                            {
+                                Console.WriteLine("Wygrana przez rząd kolumn");
+                                break;
+                            }
+                        }
+                        break;
+                    }
+            }
+           
         }
     }
 }
---
+++ TicTacToe/Scores.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class Player1
+    {
+        public int wins { get; set; }
+        public int loses { get; set; }
+    }
+
+    public class Player2
+    {
+        public int loses { get; set; }
+        public int wins { get; set; }
+    }
+
+    public class Scores
+    {
+        public Player1 Player1 { get; set; }
+        public Player2 Player2 { get; set; }
+        public int pats { get; set; }
+    }
+}
